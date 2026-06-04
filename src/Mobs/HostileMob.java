8addc0efe4f6a0b94b689f249f47fc3652c9f35a
Yanting_Wu <TalithaWu@gmail.com>
2026-06-04 14:20:06 -0400

    Fixed some Mob bugs

--- src/Mobs/HostileMob.java
+++ src/Mobs/HostileMob.java
@@ -1,34 +1,40 @@
 package Mobs;
+
 public class HostileMob {
-    public String mobName;
-    public int mobHp;
-    public boolean isDefeated;
-    // for all mobs that can be attacked by the PLAYER use this class
-    // hostile mobs can deal damage and be dealt damage to; passive mobs do not attack nor can be attacked
-    public HostileMob(String mobName, int mobHp){
-        mobName = this.mobName;
-        mobHp = this.mobHp;
+    private String mobName;
+    private int mobHp;
+    private boolean isDefeated;
+
+    public HostileMob(String mobName, int mobHp) {
+        this.mobName = mobName;
+        this.mobHp = mobHp;
+        this.isDefeated = false;
     }
     
-    public String getName(){
+    public String getName() {
         return mobName;
     }
-    public int getHp(){ 
+
+    public int getHp() { 
         return mobHp;
     }
-    public void subtractHp(int amount){
+
+    public boolean isDefeated() {
+        return isDefeated;
+    }
+
+    public void subtractHp(int amount) {
         mobHp -= amount;
-        if (mobHp <= 0){
+        if (mobHp <= 0) {
+            mobHp = 0;
             isDefeated = true;
         }
     }
-    public void HpBleedDmg(int turns, int amount){
-        // subtracts a certain amount of Hp each turn until it expires or until mob is defeated
-        for (int i = 0; i <= turns; i++){
-            mobHp -= amount;
-            if (mobHp <= 0){
-                isDefeated = true;
-        }
+
+    public void applyBleedTick(int amount) {
+        if (!isDefeated) {
+            subtractHp(amount);
+            System.out.println(mobName + " takes " + amount + " bleeding damage!");
         }
     }
-    }
+}
--- src/Mobs/PassiveMob.java
+++ src/Mobs/PassiveMob.java
@@ -1,10 +1,15 @@
 package Mobs;
+
 public class PassiveMob {
     public String mobName;
-    // for all mobs that CANNOT be attacked by the PLAYER use this class
-    // hostile mobs can deal damage and be dealt damage to; passive mobs do not attack nor can be attacked
-    public PassiveMob(String mobName, int mobHp){
-        mobName = this.mobName;
+
+    // For all mobs that CANNOT be attacked by the PLAYER use this class
+    // Hostile mobs can deal damage and be dealt damage to; passive mobs do not attack nor can be attacked
+    public PassiveMob(String mobName) {
+        this.mobName = mobName;
     }
-    public boolean isDefeated;
+
+    public String getName() {
+        return mobName;
     }
+}
